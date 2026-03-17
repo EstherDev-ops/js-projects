139a7cbebe52167ed0fd25353d27f99ade170c81
Esther Maina <mainaesther1164@gmail.com>
2026-03-17 10:16:07 +0300

    Fix Typos and add missing concepts to Day 1 notes

--- notes.js
+++ notes.js
@@ -1,6 +1,6 @@
-//let- is a js keyword used to declare a variable whoch value can change later.
-//Block Scope: Is declared using let or const and has culry braces {}. where the variable is only accessible within the block scope.
-//variable shadowing- is when a variable is decalre inside the block scope using the same name as the outer scope. JS read the closest values to where it runs;
+//let- is a js keyword used to declare a variable which value can change later.
+//Block Scope: Is declared using let or const and has curly braces {}. where the variable is only accessible within the block scope.
+//variable shadowing- is when a variable is declare inside the block scope using the same name as the outer scope. JS read the closest values to where it runs;
 
 //constants- is a js keyword that create an immutable variable. whose value cannot be reassigned or redeclared. it is also a block scope variable.
 /*let number = 10;
@@ -16,7 +16,7 @@ console.log(number);// 20
  console.log(number);
  */
 
-//Hoisting- var gets moved to the top of the scopeand initialized with undefined. example:
+//Hoisting- var gets moved to the top of the scope and initialized with undefined. example:
 
 /*var number; // declaration moves  top TOP
  number = 10; //assingment or initialization stays in place
@@ -25,10 +25,10 @@ console.log(number);// 20
 
  //Data types- Are different types ofvalues, that can be stored and manipulated by programming languages.
 
- //Numbers- used to represent nummeric values, used to store integers and floating points.
-//primititve types- Are stored by values in the stack memory, examples: numbers, boolean, strings, nulll, undefined, symbols, bugInt. are immutable, meaning their value can only br changed by creating a new value and assigning it to the variable that holds the primitive value.
+ //Numbers- used to represent numeric values, used to store integers and floating points.
+//primititve types- Are stored by values in the stack memory, examples: numbers, boolean, strings, null, undefined, symbols, BigInt. are immutable, meaning their value can only br changed by creating a new value and assigning it to the variable that holds the primitive value.
 
-//reference types- Are stored by pointers/adress, located in heap memory. are mutables, meaning their values can be change with out creating a new variable, if the point the same memorylocation: arrays, objects, functions.
+//reference types- Are stored by pointers/address, located in heap memory. are mutables, meaning their values can be change with out creating a new variable, if the point the same memorylocation: arrays, objects, functions.
 
 /*let a = 10;
 let b = a;
@@ -45,7 +45,7 @@ b.name = "Maina";
 console.log(a.name);
 console.log(b.name);*/
 
-//Why do JS made objects work by reference instead of copying just like primitive types? This would result to memory watage, slow peerfeomance and also costy. Due to that objects can be huge and copying each evrytime you pass it somewhere would result to memory inefficency.
+//Why do JS made objects work by reference instead of copying just like primitive types? This would result to memory wastage, slow peerfomance and also costy. Due to that objects can be huge and copying each everytime you pass it somewhere would result to memory inefficency.
 
 /*let a = 10;
 let b = a;
@@ -57,7 +57,7 @@ console.log(b);*/
 //In javascript additional operators has two functionality: Arithmetic and concantenation.
 //if it is used with strings either side it performs concantenation(joining the strings).This called Type Coercion- where js automatically converts one data type to another. 
 //if used with numbers it performs arithmetic expression. 
-//NOTE: The rest of the operators performs aritnmethic operation, even if used with strings, they will be coerced into numbers, if possible.
+//NOTE: The rest of the operators performs aritmethic operation, even if used with strings, they will be coerced into numbers, if possible.
 //Example:
 /*console.log("5" + 3);//53
 console.log("5"  - 3);//2
@@ -78,7 +78,7 @@ console.log(parseFloat(x));
 /*console.log(Number("5.7abc"));
 console.log(parseFloat("5.7abc"));*/
 
-//Number()- it will run but our output will be Nan, which in our case it woun't be our prefered output.
+//Number()- it will run but our output will be Nan, which in our case it won't be our prefered output.
 //parseFloat- reads values from left to right, if it hits a non-number value it stops, and prints out, the number read.
 
 /*let number = "5.7abc";
@@ -95,5 +95,8 @@ console.log(typeof null);//object*/
 
 /*let a;
 let b = null;
-console.log(a)
-console.log(b);*/
+console.log(a)//undefined
+console.log(b);//null
+*/
+//in the code above: undefined will be output in the first line of the code, since we haven't assign any value to the variable, so JavaScript automatically passes(assigns) the undefined value to the variable.
+//null will be the output, even though in simple Englisj we know that null is nothing. but here we tells JavaScript, that we are intentionally using null, as we wait for the real data to arrives, but remember that null, is a primitive data type.
